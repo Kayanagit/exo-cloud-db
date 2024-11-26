b912a5414b01d2e8d8f35f4fc24f74d52b308c01
Kayanagi <delordwesley@gmail.com>
2024-11-26 14:54:55 +0100

    voila

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'postgresql://db_client_js_eycj_user:preYS3IEZjNHgmKrAozeK4yZxfMx0SQy@dpg-ct2t5qbqf0us73a1tsv0-a.frankfurt-postgres.render.com/db_client_js_eycj', // TODO
   {
     dialectOptions: {
       ssl: {
